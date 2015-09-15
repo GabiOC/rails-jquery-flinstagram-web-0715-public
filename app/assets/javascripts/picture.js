9a98402e8f38496c9b73a8d60e14a080654d5b43
GabiOC <oconnor.gabriele@gmail.com>
2015-09-15 18:03:40 -0400

    all done

--- app/assets/javascripts/picture.js
+++ app/assets/javascripts/picture.js
@@ -1,1 +1,15 @@
 // Add your JavaScript here
+
+// $(function(){
+//   $('.like-btn').click(function(e){
+//     e.preventDefault();
+//     var destination = $(this).attr('href');
+//
+//     $.ajax({
+//       url: destination,
+//       data: {layout: false}
+//     }).done(function(html){
+//       $("#details").html(html);
+//     })
+//   })
+// })
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -7,7 +7,14 @@ class PicturesController < ApplicationController
   def like
     @picture = Picture.find(params[:id])
     @picture.update_likes(current_user)
-    redirect_to root_path
+    respond_to do |format|
+      format.html do
+        if params[:layout] == "false"
+          render layout: false
+        end
+      end
+      format.js
+      format.json { render :like }
+    end
   end
-
 end
